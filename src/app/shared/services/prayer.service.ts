8551d983796390a21f87ad6448b1cf647b7c393c
Areeb <areebaizam@gmail.com>
2023-02-23 14:02:21 -0800

    test deploy

--- src/app/shared/services/prayer.service.ts
+++ src/app/shared/services/prayer.service.ts
@@ -467,7 +467,7 @@ export class PrayerService {
     this.prayers.forEach((p) => {
       console.log(
         formatDate(Date(),"medium", this.locale),
-        " Prayers-FBGC",        
+        " Prayers-FBGC1",        
         p.name,
         p.label,
         p.start,
